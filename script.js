03b19137e3b99a9b5e7b30b4f8a91b079947acfa
CopperCoder227 <526csteiner@frhsd.com>
2024-11-12 09:30:57 -0500

    "comit this version so you hvae something to work off of"

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let userFavoriteColor = prompt('What is your favorite color?')
 let swallowAirSpeed = prompt ('What is the speed of an unlaiden swallow?')
 
 
-let myStory = `<p> Hello to our user: ${userName}! Answer me these questions 3 an the other side you'll see.</p>
+let myStory = `<p> Hello to our user: ${userName}! Answer me <span class="loud">these</span> questions 3 an the other side you'll see.</p>
 
 <p> I see you have already answered, so your quest is to ${userQuest}</p>
 `
