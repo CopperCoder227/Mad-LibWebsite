bcfd00ee3d832ed4d230c3342e73c06c20043f26
CopperCoder227 <526csteiner@frhsd.com>
2024-11-12 09:28:51 -0500

    fancy words

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 let userName = prompt('WHAT IS YOUR NAME?!?!?!')
+let userQuest = prompt('What is your quest?')
+let userFavoriteColor = prompt('What is your favorite color?')
+let swallowAirSpeed = prompt ('What is the speed of an unlaiden swallow?')
+
+
+let myStory = `<p> Hello to our user: ${userName}! Answer me these questions 3 an the other side you'll see.</p>
+
+<p> I see you have already answered, so your quest is to ${userQuest}</p>
+`
+console.log (myStory)
+
+document.getElementById('story').innerHTML = myStory
