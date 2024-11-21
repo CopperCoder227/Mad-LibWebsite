389e889dcfa59c24d070a8a819ff86b4da5192ad
CopperCoder227 <526csteiner@frhsd.com>
2024-11-21 08:56:47 -0500

    i should probaby omit now

---
+++ script3.js
@@ -0,0 +1,35 @@
+//thingys to fill out//
+let userName = sessionStorage.getItem("userName");
+let userAge = sessionStorage.getItem("userAge");
+let userColor = sessionStorage.getItem("userColor");
+let userBestie = sessionStorage.getItem("userBestie");
+let userHate = sessionStorage.getItem("userHate");
+
+//THE STORY//
+let myStory = 
+`
+<h2>Chapter 3 - Kidnapping Occurs</h2>
+<h4>
+  Once upon a time, <span class="diff">${userName}</span> wakes up in their nice warm bed. The decide to
+  call <span class="diff">${userBestie}</span> to make sure their ok, after a long night of partying
+  before.
+</h4>
+<h4>
+  When <span class="diff">${userBestie}</span> doesnt pick up their phone, <span class="diff">${userName}</span> gets concerned.
+  They use their magical hotline to directly call <span class="diff">${userBestie}</span>.
+</h4>
+<h4>
+  They use their <span class="diff">${userColor}</span> magic mirror to telepathically call their best
+  friend <span class="diff">${userBestie}</span>. When ${userBestie}</span> sill doesn't wake up, however,
+  <span class="diff">${userName}</span> cals down and assumes they just fell asleep.
+</h4>
+<h4>
+About <span class="diff2">${randomNumber}</span> days later, <span class="diff">${userName}</span> still hadn't heard from <span class="diff">${userBestie}</span> and became worried once more.
+</h4>
+<a href="pt4.html"><h4 class="GoOn1"><b>They decide to go out and find their friend!</b></h4></a>
+`
+
+
+console.log (myStory)
+
+document.getElementById('story').innerHTML = myStory
--- scrpit2.js
+++ scrpit2.js
@@ -9,22 +9,24 @@ let userHate = sessionStorage.getItem("userHate");
 let myStory = 
 `
 <h2>Chapter 2 - Destiny Awaits</h2>
-<p>
-  THEY DECIDE O DIE THE END
-</p>
-<p>
-  When ${userBestie} doesn't pick up their phone, ${userName} gets concerned.
-  They use their magical hotline to directly call ${userBestie}.
-</p>
-<p>
-  They use their ${userColor} magic mirror to telepathically call their best
-  friend ${userBestie}. When ${userBestie} sill doesn't wake up, however,
-  ${userName} calms down and assumes they just fell asleep.
-</p>
-</p>
-About _____ days later, ${userName} still hadn't heard from ${userBestie} and became extra worried again.
-</p>
-<a href="pt2.html"><h3 class="GoOn1">Continue the story?</h3></a>
+<h4>
+  Once upon a time, <span class="diff">${userName}</span> wakes up in their nice warm bed. The decide to
+  call <span class="diff">${userBestie}</span> to make sure their ok, after a long night of partying
+  before.
+</h4>
+<h4>
+  When <span class="diff">${userBestie}</span> doesnt pick up their phone, <span class="diff">${userName}</span> gets concerned.
+  They use their magical hotline to directly call <span class="diff">${userBestie}</span>.
+</h4>
+<h4>
+  They use their <span class="diff">${userColor}</span> magic mirror to telepathically call their best
+  friend <span class="diff">${userBestie}</span>. When ${userBestie}</span> sill doesn't wake up, however,
+  <span class="diff">${userName}</span> cals down and assumes they just fell asleep.
+</h4>
+<h4>
+About <span class="diff2">${randomNumber}</span> days later, <span class="diff">${userName}</span> still hadn't heard from <span class="diff">${userBestie}</span> and became worried once more.
+</h4>
+<a href="pt3.html"><h4 class="GoOn1"><b>They decide to go out and find their friend!</b></h4></a>
 `
 
 
